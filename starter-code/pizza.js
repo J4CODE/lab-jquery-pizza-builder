d315c6172d77f8df154afe7bd9913e7c46562702
Jay <luvntruth77@gmail.com>
2018-07-21 15:55:15 -0400

    MIA Ironhack Labs - Pizza Builder - Ruben

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,3 @@
-// Write your Pizza Builder JavaScript in this file.
-
 
 
 //Write your Pizza Builder JavaScript in this file.
@@ -7,56 +5,61 @@
 
 
 $(document).ready(function () {
-      console.log("ready!");
+  console.log("ready!");
 
-      $('.btn-pepperonni').data('price', 1);
-      $('.btn-mushrooms').data('price', 1);
-      $('.btn-green-peppers').data('price', 1);
-      $('.btn-sauce').data('price', 3);
-      $('.btn-crust').data('price', 5);
+  $('.btn-pepperonni').data('price', 1);
+  $('.btn-mushrooms').data('price', 1);
+  $('.btn-green-peppers').data('price', 1);
+  $('.btn-sauce').data('price', 3);
+  $('.btn-crust').data('price', 5);
 
-      function calculatePrice() {
-        var price = 10;
-        for (var i = 0; i < allTheButtons.length; i++) {
-          if (allTheButtons.eq(i).hasClass('active')) {
-            price += allTheButtons.eq(i).data('price');
-          }
-        };
 
-        $('.btn-pepperonni').click(function () {
-          $(this).toggleClass('active');
-          $(".pep").fadeToggle();
-          $('.price ul li:first').toggle();
-        });
+  function calculatePrice() {
+    var price = 10;
+    for (var i = 0; i < allTheButtons.length; i++) {
+      if (allTheButtons.eq(i).hasClass('active')) {
+        price += allTheButtons.eq(i).data('price');
+      }
+    };
 
-        $('.btn-mushrooms').click(function () {
-          $(this).toggleClass('active');
-          $(".mushroom").fadeToggle();
-          $('.price ul li:nth-child(2)').toggle();
 
-        });
+    $('.btn-pepperonni').click(function () {
+      $(this).toggleClass('active');
+      $(".pep").fadeToggle();
+      $('.price ul li:first').toggle();
+    });
 
-        $('.btn-green-peppers').click(function () {
-          $(this).toggleClass('active');
-          $(".green-pepper").fadeToggle();
-        });
+    $('.btn-mushrooms').click(function () {
+      $(this).toggleClass('active');
+      $(".mushroom").fadeToggle();
+      $('.price ul li:nth-child(2)').toggle();
 
-        $('.btn-sauce').click(function () {
-          $(this).toggleClass('active');
-          $(".sauce-white").fadeToggle();
-        });
+    });
 
-        $('.btn-crust').click(function () {
-          $(this).toggleClass('active');
-          $(".crust").toggleClass('crust-gluten-free');
-        });
+    $('.btn-green-peppers').click(function () {
+      $(this).toggleClass('active');
+      $(".green-pepper").fadeToggle();
+    });
 
-        $('.btn-sauce').click(function () {
-          $(this).toggleClass('active');
-          $(".sauce").toggleClass('sauce-white');
-        });
+    $('.btn-sauce').click(function () {
+      $(this).toggleClass('active');
+      $(".sauce-white").fadeToggle();
+    });
 
-        $('.btn-crust').removeClass('active');
-        $('.btn-sauce').removeClass('active');
+    $('.btn-crust').click(function () {
+      $(this).toggleClass('active');
+      $(".crust").toggleClass('crust-gluten-free');
+    });
 
-      });
+    $('.btn-sauce').click(function () {
+      $(this).toggleClass('active');
+      $(".sauce").toggleClass('sauce-white');
+    });
+
+
+    $('.btn-crust').removeClass('active');
+    $('.btn-sauce').removeClass('active');
+
+
+  });
+
